578591bd6af5edba90a01bba5e18834ea5e095fe
Sean Kell <sean.michael.kell@gmail.com>
2023-03-02 20:21:47 -0500

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    let args: Vec<String> = env::args().collect();
+    if args[1] == "init" {
+        if args.len() >= 2 {
+            let p = std::path::Path::new(&args[2]);
+            env::set_current_dir(p).unwrap();
+        }
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+        fs::create_dir(".git").unwrap();
+        fs::create_dir(".git/objects").unwrap();
+        fs::create_dir(".git/refs").unwrap();
+        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+        println!("Initialized git directory")
+    } else {
+        println!("unknown command: {}", args[1])
+    }
 }
