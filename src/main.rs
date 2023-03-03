058779a8c2ff57c01662dd32aae6b5ea0d8b60f2
Sean Kell <sean.michael.kell@gmail.com>
2023-03-02 21:41:40 -0500

    fix: fix .git/HEAD init

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,7 @@ fn run_init(directory: &Option<String>) -> Result<(),io::Error> {
         fs::create_dir(".git")?;
         fs::create_dir(".git/objects")?;
         fs::create_dir(".git/refs")?;
-        fs::write(".git/HEAD", "refs/heads/master\n")?;
+        fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
     }
 
     Ok(())
